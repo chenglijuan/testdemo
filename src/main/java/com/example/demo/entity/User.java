0ef5e860c661f50b7969fff79aa3a3370d2352c7
chenglijuan <18297788856@163.com>
2022-08-30 21:42:47 +0800

    tijiao

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+/**
+ * @Author: chenglijuan
+ * @Data: 2022/8/30  下午9:04
+ * @Decription:
+ * @Modified:
+ */
+@Data
+public class User {
+
+    private String name;
+
+    private int age;
+
+    private String userName;
+
+    private String pwd;
+
+
+}
